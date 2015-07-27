d7c8ab577bf7254632164e410577b113834e8df8
Jonathan Gertig <jcgertig@gmail.com>
2015-07-27 18:19:46 -0400

    changes to extend

--- src/websocket_rails/websocket_rails.js
+++ src/websocket_rails/websocket_rails.js
@@ -30,7 +30,7 @@ var __bind = function(fn, me){
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) {
       for (var key in parent) {
-        if (!__hasProp.call(parent, key)) {
+        if (!parent.hasOwnProperty(key)) {
           child[key] = parent[key];
         }
       }
